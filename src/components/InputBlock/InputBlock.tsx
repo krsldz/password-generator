9868f8fa9e524a667eebbb40f85ad13463c84f91
Kris <kristina.eldzarova@gmail.com>
2023-06-17 12:15:19 +0400

    02 copy logic added

--- src/components/InputBlock/InputBlock.tsx
+++ src/components/InputBlock/InputBlock.tsx
@@ -1,13 +1,18 @@
 import React, { type FC, memo } from 'react';
 import styles from './InputBlock.module.css';
 
-const InputBlock: FC = () => {
+const InputBlock: FC<{
+  onCopy: () => void;
+  value: string;
+}> = ({ onCopy, value }) => {
   const link = () => console.info('hi');
 
   return (
     <div className={styles.wrap}>
-      <input />
-      <button>copy</button>
+      <input readOnly value={value} />
+      <button onClick={onCopy} type="button">
+        copy
+      </button>
       <button>generate</button>
     </div>
   );
--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,11 +1,18 @@
-import React, { type FC, memo } from 'react';
+import React, { type FC, memo, useState, useCallback, FormEvent } from 'react';
+import copy from 'copy-to-clipboard';
 import ControlCenter from '../ControlCenter/ControlCenter';
 import InputBlock from '../InputBlock/InputBlock';
 
 import styles from './MainPage.module.css';
 
 const MainPage: FC = () => {
-  const link = () => console.info('hi');
+  const [inputValue, setInputValue] = useState<string>('');
+
+  const handleCopy = useCallback((): void => {
+    copy(inputValue, {
+      format: 'text/plain',
+    });
+  }, [inputValue]);
 
   return (
     <div className={styles.header}>
@@ -14,7 +21,7 @@ const MainPage: FC = () => {
           <span className={styles.mainTitle}>Keep it safe</span>
           <span className={styles.subTitle}>password generator</span>
         </h1>
-        <InputBlock />
+        <InputBlock onCopy={handleCopy} value={inputValue} />
         <ControlCenter />
       </div>
     </div>
